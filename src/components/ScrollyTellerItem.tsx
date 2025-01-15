6172a9edc93c5aa222e243fd580ef2409588709a
Johan Hellgren <johan.hellgren@unicus.com>
2025-01-15 13:52:21 +0100

    Implement page-wide smooth scroll

--- src/components/ScrollyTellerItem.tsx
+++ src/components/ScrollyTellerItem.tsx
@@ -20,7 +20,20 @@ const ScrollyTellerItem = ({
   const [headerMargin, setHeaderMargin] = useState(0);
   const [subheaderMargin, setSubheaderMargin] = useState(0);
   const margin = 15;
-  const locomotiveScroll = new LocomotiveScroll();
+  const locomotiveScroll = new LocomotiveScroll({
+    lenisOptions: {
+        wrapper: window,
+        content: document.documentElement,
+        lerp: 0.1,
+        duration: 1.2,
+        orientation: 'vertical',
+        gestureOrientation: 'vertical',
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    },
+});
 
   useEffect(() => {
     // Set header and subheader margins to prevent them from ever overlapping
