6fb9c2d55d1e281334828a6c7c273343b643d096
Johan Hellgren <honajnahoj@gmail.com>
2025-01-20 19:47:55 +0100

    refactor to use sass

--- src/components/ScrollyTellerItem.tsx
+++ src/components/ScrollyTellerItem.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import LocomotiveScroll from 'locomotive-scroll'
+import { useEffect, useRef, useState } from 'react'
+import '../styles/components/_scrolly-teller-item.scss'
 import ScrollyTellerItemImage, {
   ScrollyTellerItemImageProps,
-} from "./ScrollyTellerItemImage";
-import LocomotiveScroll from "locomotive-scroll";
+} from './ScrollyTellerItemImage'
 
 export interface ScrollyTellerItemProps {
-  header: string;
-  subheader: string;
-  images: ScrollyTellerItemImageProps[];
+  header: string
+  subheader: string
+  images: ScrollyTellerItemImageProps[]
 }
 
 const ScrollyTellerItem = ({
@@ -15,70 +16,67 @@ const ScrollyTellerItem = ({
   subheader,
   images,
 }: ScrollyTellerItemProps) => {
-  const headerRef = useRef<HTMLHeadingElement>(null);
-  const subheaderRef = useRef<HTMLHeadingElement>(null);
-  const [headerMargin, setHeaderMargin] = useState(0);
-  const [subheaderMargin, setSubheaderMargin] = useState(0);
-  const margin = 15;
+  const headerRef = useRef<HTMLHeadingElement>(null)
+  const subheaderRef = useRef<HTMLHeadingElement>(null)
+  const [headerMargin, setHeaderMargin] = useState(0)
+  const [subheaderMargin, setSubheaderMargin] = useState(0)
+  const margin = 15
   const locomotiveScroll = new LocomotiveScroll({
     lenisOptions: {
       wrapper: window,
       content: document.documentElement,
       lerp: 0.1,
       duration: 1.2,
-      orientation: "vertical",
-      gestureOrientation: "vertical",
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
       smoothWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
     },
-  });
+  })
 
   useEffect(() => {
     const handleResize = () => {
       // Set header and subheader margins to prevent them from ever overlapping
       if (headerRef.current && subheaderRef.current) {
-        const headerHeight = headerRef.current.offsetHeight;
-        const subheaderHeight = subheaderRef.current.offsetHeight;
-        setHeaderMargin(subheaderHeight + margin);
-        setSubheaderMargin(headerHeight + margin);
+        const headerHeight = headerRef.current.offsetHeight
+        const subheaderHeight = subheaderRef.current.offsetHeight
+        setHeaderMargin(subheaderHeight + margin)
+        setSubheaderMargin(headerHeight + margin)
       }
-    };
+    }
+    handleResize()
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [header, subheader]);
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [header, subheader])
 
   return (
-    <div className="relative min-h-screen flex p-[30px]">
-      <div className="w-1/2 flex flex-col">
+    <div className="scrolly-teller-item">
+      <div className="scrolly-teller-item__column">
         <div
-          className="sticky top-[30px]"
+          className="scrolly-teller-item__header"
           style={{
             marginBottom: headerMargin,
           }}
         >
-          <h1 ref={headerRef} className="text-9xl font-bold text-[#143275]">
-            {header}
-          </h1>
+          <h1 ref={headerRef}>{header}</h1>
         </div>
-        <div className="flex-grow" />
+        <div className="scrolly-teller-item__column--content" />
         <div
-          className="sticky bottom-[30px]"
+          className="scrolly-teller-item__subheader"
           style={{
             marginTop: subheaderMargin,
           }}
         >
-          <h2 ref={subheaderRef} className="text-8xl text-[#143275]">
-            {subheader}
-          </h2>
+          <h2 ref={subheaderRef}>{subheader}</h2>
         </div>
       </div>
-      <div className="w-1/2">
+      <div className="scrolly-teller-item__column">
         {images.map((image, index) => (
           <ScrollyTellerItemImage
             key={image.imageUrl}
@@ -88,7 +86,7 @@ const ScrollyTellerItem = ({
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ScrollyTellerItem;
+export default ScrollyTellerItem
